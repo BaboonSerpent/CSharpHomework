62b3954391ec8d4228db33814f1ca262067d12f1
BaboonSerpent <634582156@qq.com>
2019-03-20 21:13:24 +0800

    zuoye3

---
+++ Homework3/AbstractEncryptFacade/抽象外观类/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+namespace FacadeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractEncryptFacade ef; //针对抽象外观类编程
+                                      //读取配置文件
+            string facadestring = Configurationmanager.Appsettings["facade"];
+            ///反射生成对象
+            ef = (AbstractEncryptFacade)Assembly.Load("FacadeSample").CreateInstance(facadeString);
+            ef.FileEncrypt("src.txt", "des.txt");
+            Console.Read();
+        }
+    }
+}
---
+++ Homework3/EncryptFacade/FileReader/FileOp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FacadeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptFacade ef = new EncryptFacade();
+            ef.FileEncrypt("src.txt", "des.txt");
+            Console.Read();
+        }
+    }
+}
